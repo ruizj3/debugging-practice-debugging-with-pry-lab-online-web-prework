c968fa0eaaae23d1cbb630804a436e2f164055a7
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-18 00:42:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -1,3 +1,7 @@
+petrol_petes = GasStation.new("Petrol Pete's", 50)
+seashore_shell = GasStation.new("The Seashore Shell", 40)
+dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
+
 class GasStation
 
   attr_reader :brand, :unleaded_price
@@ -8,7 +12,3 @@ class GasStation
   end
 
 end
-
-petrol_petes = GasStation.new("Petrol Pete's", 50)
-seashore_shell = GasStation.new("The Seashore Shell", 40)
-dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
