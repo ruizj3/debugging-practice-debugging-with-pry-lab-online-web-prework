360b738d6f88c9185a8a7ee7351abbd5f90b8052
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-18 00:54:34 +0000

    Done.

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,9 +1,10 @@
+
 class Person
   attr_accessor :name, :candy_lover
 
   def initialize(name, candy_lover)
     @name = name
-    @candy_lover = candy_lover
+    @candy_lover == candy_lover
   end
 
   def tall?
@@ -18,12 +19,13 @@ class Person
     true
   end
 
-  def ballin_candy_lover?
-    if @candy_lover == true
-      "Aw sweet, this cat loves them some candy!"
-    else
-      10.times do
-        "not a ballin candy lover"
-    end
+   def ballin_candy_lover?
+     if candy_lover = true
+       "Aw sweet, this cat loves them some candy!"
+     else
+       10.times do
+         "not a ballin candy lover"
+     end
   end
+end
 end
